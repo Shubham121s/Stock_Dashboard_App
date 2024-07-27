ef1544debf3ca8336bb5b8cfcead5ccb74cc8479
Shubham121s <sp6063512@gmail.com>
2024-07-27 09:39:30 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
